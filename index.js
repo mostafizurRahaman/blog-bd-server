c3c1c60941add0aa40eddb1383cdec0af42ed94a
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2023-03-30 03:12:47 +0600

    feat: create apies for blogs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -58,6 +58,46 @@ const run = async () => {
             Users API's End
       =======================
       */
+
+      /* =====================
+         JWT Token section:-
+      =======================*/
+
+      /* =====================
+         JWT Token Section end :-
+      ======================= */
+
+      /* =====================
+         Blog's Start:-
+      =======================*/
+      const BlogCollections = database.collection('blogs');
+      app.get('/blogs', async(req,res)=>{
+         const query = {}; 
+         const cursor = BlogCollections.find(query).sort({date: -1}); 
+         const blogs =await  cursor.toArray()
+         res.send(blogs);
+      })
+
+
+     app.get('/blogs/:id', async(req,res)=>{
+       const {id} = req.params; 
+       const query ={
+         _id: new ObjectId(id),
+       }
+       const blog = await BlogCollections.findOne(query);
+       res.send(blog);
+       
+     })
+
+      app.post('/blogs', async(req,res)=>{
+         const {title, description, email, author, date, image} = req.body; 
+         const result = await BlogCollections.insertOne({title, description, email, author, date, image}); 
+         res.send(result); 
+      })
+
+      /*=====================
+         Blog's end :-
+      ======================= */
    } finally {
    }
 };
